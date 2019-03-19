b94207fec2c09facb5152845d7538e26a89e0bda
Ben Ragsdale <ben.ragsdale@gmail.com>
2019-03-19 14:14:25 -0500

    added pg stuff to server.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const app = express();
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function(){
+    console.log('App listening at port', PORT);
+    
+})
+
+//set up pg to talk with our songs db
+
+const Pool = pg.Pool;
+const pool = new Pool({
+database: 'atbash-songs', //you will change this for EACH app, rest of can be the same 
+host: 'localhost',
+port: 5432, 
+max: 10, //max connections in pool
+idleTimeoutMillis: 30000 // 30 seconds before end of query
+});
+
+pool.on('connect', () => {
+console.log('Postgres connected!');
+});
+
+pool.on('error', (error) => {
+    console.log('The database has an error:', error);
+})
